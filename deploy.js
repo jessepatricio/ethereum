8cc513dd730306339551329c1962f2b693398c50
Jesse Patricio <jessepatricio@gmail.com>
2018-05-23 19:45:32 +1200

    initial commit

---
+++ deploy.js
@@ -0,0 +1,30 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'almost rescue garment ethics song derive damage roast flat purchase sauce cash',
+    'https://rinkeby.infura.io/03KTZdQNcNktfqpm3UV7'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log(interface);
+    console.log('Contract deployed to', result.options.address);
+
+};
+
+deploy();
+
+
+
+
+
